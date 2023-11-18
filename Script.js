b3442aee20982b1cf8e4e7d8db8a4abbf137c286
Dharmik jain <134823823+DJKILLEER@users.noreply.github.com>
2023-11-18 11:08:59 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -5,12 +5,25 @@ const weather = document.querySelector("#weather")
     // const API = `https://api.openweathermap.org/data/2.5/weather?
     // q=${city}&appid=${API_KEY}&units=metric`
     // const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+// const getWeather = async(city) => {
+//     weather.innerHTML = `<h2> Loading... <h2>`
+//     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+//     const response = await fetch(url);
+//     const data = await response.json()
+//     return showWeather(data)
+// }
 const getWeather = async(city) => {
-    weather.innerHTML = `<h2> Loading... <h2>`
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-    const response = await fetch(url);
-    const data = await response.json()
-    return showWeather(data)
+    try{
+        weather.innerHTML = `<h2> Loading... <h2>`
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+        const response = await fetch(url);
+        const data = await response.json()
+        return showWeather(data)
+    }
+    catch(e){
+         weather.innerHTML= `<h2> Failed..</h2>`
+    }
+   
 }
 
 const showWeather = (data) => {
